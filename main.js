2120201bc83f691d50e59274e5bbfd90b8e260c8
dran5447 <danielle.r.neuberger@gmail.com>
2019-06-04 22:54:11 -0700

    Add search capability

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 angular.module('booksApp', [])
   .controller('MainController', function($scope, $http) {
+    const sectionHeaderOptions = {
+        DEFAULT: "Top Picks for You",
+        SEARCH: "Results"
+    }
+    var appController = this;
+    
     var favorites = [];
     $scope.bookList = [];    
+    $scope.sectionHeader = sectionHeaderOptions.DEFAULT;
  
-    search = function() {
-        //TODO
+    $scope.search = function() {
+        var key = constants.API_ENDPOINT;
+        var query = $scope.searchText;
+
+        if(query && query!==""){
+            $scope.sectionHeader = sectionHeaderOptions.SEARCH;
+            var requestUrl = "https://www.googleapis.com/books/v1/volumes?q="+query+"&key="+key;
+
+            $http({
+                method: 'GET',
+                url: requestUrl
+            }).then(function successCallback(response) {
+                $scope.bookList = response.data.items;
+            }, function errorCallback(response) {
+                //TODO show error
+            });    
+        }
+    };
+
+    $scope.filter = function(){
+        //TODO enable some filtering options
+
         // ordered format -   //https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=yourAPIKey
         //by id - https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=yourAPIKey
-    };
+    }
 
     loadBooks = function(){
         var key = constants.API_ENDPOINT;
